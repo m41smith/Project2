4fadfccb9afe89b0c9c9a8e255f2acfa9243c0d3
m41smith <m41smith@student.bridgew.edu>
2021-03-15 13:57:24 -0400

    Working prototype 2

--- Project2.py
+++ Project2.py
@@ -4,6 +4,7 @@ import random
 
 def main():
     win = graphics.GraphWin("Drawing Shapes", 720, 480)
+    cList = ['red', 'blue', 'azure', 'brown', 'cyan', 'DarkGray', 'DeepPink', 'gold', 'LimeGreen', 'orange']
     num = int(input("How many shapes do you want?"))
     tShape = 0
     while tShape < num:
@@ -15,6 +16,7 @@ def main():
             fy = int(input("Give the first Y coordinate of the Rectangle."))
             ly = int(input("Now give the second Y coordinate of the Rectangle."))
             r = graphics.Rectangle(graphics.Point(fx, fy), graphics.Point(lx, ly))
+            r.setFill(cList[random.randint(0,9)])
             r.draw(win)
             tShape = tShape + 1
 
@@ -24,6 +26,7 @@ def main():
             fy = int(input("Give the first Y coordinate of the Oval."))
             ly = int(input("Now give the second Y coordinate of the Oval."))
             o = graphics.Oval(graphics.Point(fx, fy), graphics.Point(lx, ly))
+            o.setFill(cList[random.randint(0,9)])
             o.draw(win)
             tShape = tShape + 1
 
@@ -32,6 +35,7 @@ def main():
             cy = int(input("Give the center Y coordinate for the Circle."))
             radius = int(input("Now Give the Radius of the Circle."))
             c = graphics.Circle(graphics.Point(cx, cy), radius)
+            c.setFill(cList[random.randint(0,9)])
             c.draw(win)
             tShape = tShape + 1
 
@@ -42,6 +46,7 @@ def main():
             ly = int(input("Now give the second Y coordinate of the Line."))
             l = graphics.Line(graphics.Point(fx, fy), graphics.Point(lx, ly))
             l.setWidth(2)
+            l.setOutline(cList[random.randint(0,9)])
             l.draw(win)
             tShape = tShape + 1
 
@@ -57,6 +62,7 @@ def main():
             x = int(input("Give the X coordinate of the Point."))
             y = int(input("Now give the Y coordinate of the Point."))
             p = graphics.Point(x, y)
+            p.setOutline(cList[random.randint(0,9)])
             p.draw(win)
             tShape = tShape + 1
 
